41dffcfdf29f623955d22634c9a1a83418f30868
Farkhat Nizaev <farhat.nizaev@yandex.ru>
2021-11-21 00:41:21 +0300

    4 проектная работа.

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const editButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const popupCloseButton = popup.querySelector(".popup__close-icon");
+const popupOverlay = popup.querySelector(".popup__overlay");
+let formElement = document.querySelector(".form");
+let nameInput = formElement.querySelector(".form__input_user_name");
+let jobInput = formElement.querySelector(".form__input_user_job");
+let nameUser = document.querySelector(".profile__info-name");
+let jobUser = document.querySelector(".profile__info-job");
+
+function open() {
+  nameInput.value = nameUser.textContent;
+  jobInput.value = jobUser.textContent;
+  popup.classList.add("popup_opened");
+}
+
+function close() {
+  popup.classList.remove("popup_opened");
+}
+
+editButton.addEventListener('click', open);
+popupCloseButton.addEventListener('click', close);
+popupOverlay.addEventListener('click', close);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  nameUser.textContent = nameInput.value;
+  jobUser.textContent = jobInput.value;
+  close();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
